1e14f294a84a21d6fddf0b409995155458a477e2
AbdlrhmanSaid <bodesaid3@gmail.com>
2023-04-16 17:32:28 +0200

    added edit

--- main.js
+++ main.js
@@ -14,3 +14,27 @@ btn.onclick = function () {
     behavior: "smooth",
   });
 };
+
+const links = document.querySelectorAll(".link");
+const localStorageKey = "activeLink";
+
+links.forEach((link) => {
+  link.addEventListener("click", function () {
+    // إزالة الكلاس من جميع الروابط
+    links.forEach((l) => l.classList.remove("activ"));
+    // إضافة الكلاس إلى الرابط المحدد
+    this.classList.add("activ");
+    // حفظ الرابط المحدد في localStorage
+    localStorage.setItem(localStorageKey, this.textContent);
+  });
+});
+
+// عرض الرابط النشط السابق (إذا كان موجودًا) عند تحميل الصفحة
+const activeLink = localStorage.getItem(localStorageKey);
+if (activeLink) {
+  links.forEach((link) => {
+    if (link.textContent === activeLink) {
+      link.classList.add("activ");
+    }
+  });
+}
